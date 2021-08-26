ae9e885f1cd0063530930429493055ddf01bf846
Kevin Burrafato <kevin.burrafato@gmail.com>
2021-08-26 17:18:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#My Portfolio
+
+This is my portfolio. 
+
+I will add my future works in the Projects section. 
+
+Stay tuned.
+
+Kevin
---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Hamburger Menu
+document.querySelector('.navbar-toggler').addEventListener('click', function () {
+
+    document.querySelector('.animated-icon1').classList.toggle('open');
+    });
+// End Hamburger Menu
+
+ 
